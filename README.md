765df8ce9f1599217003fc08b75f8f0520363692
Silvio Traversaro <silvio@traversaro.it>
2020-02-24 09:57:19 +0100

    Add first prototype of drake-lite-cmake with  CI (#1)

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
 # drake-lite-cmake
-Reduced (lite) unofficial version of drake (https://drake.mit.edu/) that can be built with CMake.  
+
+Reduced (lite) unofficial version of drake (https://drake.mit.edu/) that can be built with CMake.
+
+**Disclaimer:** drake-lite-cmake is an independent and unofficial project and is not related by any means to Toyota Research Instiute and MIT.
+
+
+# Overview
+- [Background](#background)
+- [Dependencies](#dependencies)
+- [Build and link the library](#build-and-link-the-library)
+- [Test the library](#test-the-library)
+- [API Documentation](#api-documentation)
+
+
+# Background
+[`drake`](https://github.com/RobotLocomotion/drake) is a "collection of tools for analyzing the dynamics of our robots and building control systems for them, with a heavy emphasis on optimization-based design/analysis".
+It is C++ library that contains unique features such as a [Simulink-inspired description of interconnected dynamical systems](https://drake.mit.edu/doxygen_cxx/group__systems.html) and [newly developed contact simulation algorithms specifically optimized for robot manipulation](https://drake.mit.edu/doxygen_cxx/group__contact__engineering.html).
+
+However, it is currently (as of early 2020) [distributed upstream](https://drake.mit.edu/installation.html) only with all its features enabled,
+requering the installation of [a large number of system packages](https://github.com/RobotLocomotion/drake-external-examples/tree/master/drake_cmake_installed), requiring the use of its own [vendored](https://stackoverflow.com/questions/26217488/what-is-vendoring) dependencies and supporting only a limited number of Operating Systems.
+
+This repo contains a CMake script that builds an limited subset (lite) of Drake, using CMake to find or install dependencies, ensuring cross-platform compatibility and easy integration with existing CMake workflows.
+
+The subset of Drake currently included is the one concerning the simulation of interconnected dynamical systems (`drake::systems` namespace) and the simulation of mechanical systems with contacts.
+In particular, this subset depends just on `fmt`, `Eigen3`, `fcl` and other small self-contained C++ libraries.
+
+All the parts of drake related to communication (that depends on the `lcm` library), Python bindings (the `pydrake` library) and that depend on proprietary dependency (such as the optimization infrastructure) are **not** part of `drake-lite-cmake`, and if you are interested in those you need to use the official Drake installing it from the [official instructions](https://drake.mit.edu/installation.html).
+
+For check if your Drake-based code can work with `drake-lite-cmake`, compare the headers that you used in your code the exact headers included in each version of `drake-lite-cmake`, available in the release notes.
+
+**Note that this is an unofficial and experimental project, and it provides no guarantees to work. In particular, if you find any problem using `drake-lite-cmake`, do not report them to drake upstream, as
+the problem could be induced by the modifications done at the `drake-lite-cmake` level.**
+
+# Dependencies
+drake-lite-cmake depends on
+ - [Eigen3](http://eigen.tuxfamily.org) - `version >= 3.3.3`
+ - [Flexible Collision Library (fcl)](https://github.com/flexible-collision-library/fcl) - `version >= 0.6.0`
+ - [fmt](https://fmt.dev) - `version >= 4.0.0`
+
+
+# Build and link the library
+Use the following commands to build, install and link the library, assuming that the required dependencies can be found by CMake.
+
+### Build
+With `make` facilities:
+```bash
+$ git clone https://github.com/traversaro/drake-lite-cmake
+$ cd drake-lite-cmake
+$ mkdir build && cd build
+$ cmake ..
+$ make
+$ [sudo] make install
+```
+
+With IDE build tool facilities:
+```bash
+$ git clone https://github.com/traversaro/drake-lite-cmake
+$ cd drake-lite-cmake
+$ mkdir build && cd build
+$ cmake ..
+$ cmake --build . --target ALL_BUILD --config Release
+$ cmake --build . --target INSTALL --config Release
+```
+
+See [CGold guide](https://cgold.readthedocs.io/en/latest/first-step.html) if you need more details on how to build a CMake project.
+
+### Link
+Once the library is installed, you can link it using `CMake` with as little effort as writing the following line of code in your project `CMakeLists.txt`:
+```cmake
+...
+find_package(drake-lite-cmake REQUIRED)
+...
+target_link_libraries(<target> drake-lite-cmake::drake-lite-cmake)
+...
+```
+
+# Test the library
+`drake-lite-cmake` can run a sample of Drake's tests to make sure that the library is working correctly, to run them just enable the `BUILD_TESTING` CMake option, compile the project and run:
+```sh
+$ ctest [-VV]
+```
+to run all the tests.
+
+
+# API documentaion
+For API documentation, the users should refer to the official Drake documentation available at https://drake.mit.edu/, at least for the headers and the versions that are distributed with `drake-lite-cmake`.
+
---
+++ scripts/generate-source-files.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python2.7
+#
+# A script to generate to extract the C++ source code corresponding to a set of bazel C++ targets.
+# Loosely based on https://github.com/lizan/bazel-cmakelists
+#
+# Usage:
+# $ generate-source-files.py --targets //math //systems/framework
+#
+
+import argparse
+from os import path
+import os
+import re
+import xml.etree.ElementTree as ET
+import subprocess
+import sys
+
+EXTERNAL_PATTERN = re.compile("^@(.*)\\/\\/")
+SOURCE_EXTENSION = set([".h", ".hpp", ".hh", ".hxx", ".c", ".cc", ".cpp", ".cxx"])
+
+def QueryTargets():
+  return ' union '.join(FLAGS.targets)
+
+def Exists(fn):
+  try:
+    os.stat(fn)
+    return True
+  except OSError:
+    sys.stderr.write("Skipping %s ...\n" % fn)
+    return False
+
+def GetBasePath(fn):
+  fn = fn.replace('/:', '/')
+  fn = fn.replace(':', '/')
+  if EXTERNAL_PATTERN.match(fn):
+    fn = EXTERNAL_PATTERN.sub("external/\\1/", fn)
+  return fn.lstrip('/')
+
+def ConvertGeneratedPath(fn):
+  bazel_root = 'bazel-' + FLAGS.project
+  genfiles_root = 'bazel-genfiles'
+  return path.realpath(path.join(genfiles_root, GetBasePath(fn)))
+
+def ConvertExternalPath(fn):
+  bazel_root = 'bazel-' + FLAGS.project
+  if EXTERNAL_PATTERN.match(fn):
+    return path.join(bazel_root, GetBasePath(fn))
+  return path.realpath(GetBasePath(fn))
+
+def run_bazel(args):
+  sa = os.getenv("BAZEL_STARTUP_ARGS")
+  if sa is not None:
+    args = sa.split() + args
+
+  args = ['bazel'] + args
+  print args
+  return subprocess.check_output(args)
+
+def ExtractSources():
+  query = 'kind("source file", deps(kind("cc_.* rule", deps(kind("cc_.* rule", %s)))))' % QueryTargets()
+  sources = []
+  for fn in run_bazel(['query', '--noimplicit_deps', query]).splitlines():
+    if (not FLAGS.external) and EXTERNAL_PATTERN.match(fn):
+      continue
+    fn = GetBasePath(fn)
+    if path.splitext(fn)[1] in SOURCE_EXTENSION:
+      sources.append(fn)
+  return [source for source in sources if Exists(source)]
+
+def ExtractGenerated():
+  query = 'kind("generated file", deps(kind("cc_.* rule", deps(kind("cc_.* rule", %s)))))' % QueryTargets()
+  sources = []
+  for fn in run_bazel(['query', '--noimplicit_deps', query]).splitlines():
+    fn = ConvertGeneratedPath(fn)
+    if path.splitext(fn)[1] in SOURCE_EXTENSION:
+      sources.append(fn)
+  return [source for source in sources if Exists(source)]
+
+def ExtractIncludes():
+  query = 'kind(cc_library, deps(kind("cc_.* rule", %s)))' % QueryTargets()
+  includes = []
+  xml = run_bazel(['query', '--noimplicit_deps', query, '--output', 'xml'])
+  tree = ET.fromstring(xml)
+  for e in tree.findall(".//list[@name='includes']/.."):
+    prefix = e.attrib['name'].split(':')[0]
+    for val in [i.attrib['value'] for i in e.findall("list[@name='includes']/string")] + ["."]:
+      geninc = path.realpath(path.join(ConvertGeneratedPath(prefix), val))
+      includes.append(geninc)
+
+      inc = path.realpath(path.join(ConvertExternalPath(prefix), val))
+      includes.append(inc)
+
+  for e in tree.findall(".//string[@name='include_prefix']/.."):
+    prefix = e.attrib['name'].split(':')[0]
+    include_prefix = e.find("string[@name='include_prefix']").attrib['value']
+    geninc = path.realpath(path.join(ConvertGeneratedPath(prefix), val))
+    if geninc.endswith(include_prefix):
+      geninc = geninc[:-len(include_prefix)]
+      includes.append(geninc)
+    inc = path.realpath(path.join(ConvertExternalPath(prefix), val))
+    if inc.endswith(include_prefix):
+      inc = inc[:-len(include_prefix)]
+      includes.append(inc)
+
+  for d in os.listdir('bazel-%s/external' % FLAGS.project):
+    if os.path.isdir('bazel-%s/external/%s' % (FLAGS.project, d)):
+      includes.append(path.realpath('bazel-%s/external/%s' % (FLAGS.project, d)))
+    if os.path.isdir('bazel-genfiles/external/%s' % d):
+      includes.append(path.realpath('bazel-genfiles/external/%s' % d))
+
+  return set(includes)
+
+def ExtractDefines():
+  query = 'attr("defines", "", deps(kind("cc_.* rule", %s)))' % QueryTargets()
+  xml = run_bazel(['query', '--noimplicit_deps', query, '--output', 'xml'])
+  tree = ET.fromstring(xml)
+  defines = []
+  for e in tree.findall(".//list[@name='defines']/string"):
+    defines.append(e.attrib['value'])
+  return set(defines)
+
+def ExtractCopts():
+  query = 'attr("copts", "", deps(kind("cc_.* rule", %s)))' % QueryTargets()
+  xml = run_bazel(['query', '--noimplicit_deps', query, '--output', 'xml'])
+  tree = ET.fromstring(xml)
+  copts = []
+  for e in tree.findall(".//list[@name='copts']/string"):
+    copts.append(e.attrib['value'])
+  return set(copts)
+
+def GenerateSourceFilesCMake():
+  if not FLAGS.skip_build:
+    bazel_args = ['build']
+    bazel_args.extend(FLAGS.targets)
+    bazel_args.extend(FLAGS.bazel_args[1:])
+    run_bazel(bazel_args)
+
+  sources = ExtractSources()
+  generated = ExtractGenerated()
+  includes = ExtractIncludes()
+  defines = ExtractDefines()
+  copts = ExtractCopts()
+  for opt in copts:
+    if opt.startswith("-I-"):
+      includes.add(opt[3:])
+    elif opt.startswith("-I"):
+      includes.add(opt[2:])
+    elif opt.startswith("-D"):
+      defines.add(opt[2:])
+
+  output = FLAGS.output
+  file_root = run_bazel(['info', 'workspace']).strip()
+  output = path.join(file_root, FLAGS.output)
+  try:
+    os.remove(output)
+  except:
+    pass
+  with open(output, 'w') as cmakelist:
+    cmakelist.write("set(SOURCE_FILES\n    ")
+    cmakelist.write("\n    ".join(sources))
+    cmakelist.write(")\n\n")
+
+  sys.stderr.write("source_files.cmake generated in following directory:\n")
+  sys.stderr.write(file_root + "\n")
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Generate CMakeLists.txt for IDEs from bazel targets")
+  parser.add_argument('--project')
+  parser.add_argument('--targets', default=['//...'], nargs='+')
+  parser.add_argument('--skip_build', action='store_true', help="Skip build step if you have aleady built target(s)")
+  parser.add_argument('--external', action='store_true', help='Generate project with external')
+  parser.add_argument('--output', default="source_files.cmake")
+  parser.add_argument('bazel_args', nargs=argparse.REMAINDER)
+  FLAGS = parser.parse_args()
+
+  if FLAGS.project is None:
+      prj = run_bazel(['info', 'workspace']).strip()
+      FLAGS.project = os.path.basename(prj)
+
+  GenerateSourceFilesCMake()
